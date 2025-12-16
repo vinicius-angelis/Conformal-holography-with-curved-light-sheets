ea2cc2eb5d189afa60ff6fcbe42abcfb6dad3e38
Vinicius de Angelis <131720242+vinicius-angelis@users.noreply.github.com>
2025-12-16 14:45:00 -0300

    Update field_fwbg.m

--- field_fwbg.m
+++ field_fwbg.m
@@ -7,8 +7,6 @@ phig = atan2((rho*sin(phi)-rho0*sin(phi0)),(rho*cos(phi)-rho0*cos(phi0)));
 
 betan(in) = Q+(2*pi/L).*n;
 krhon(in) = sqrt(2)*sqrt(1-(1/K)*(betan))*K;
-%krhon(in) = sqrt(K^2-(betan).^2);
-
 
 mu(z) = 1-1i*2*(q^2)*(1/K)*z;
 
@@ -16,5 +14,6 @@ aux1(in) = besselj(u,krhon.*rhog./mu).*exp(1i.*(krhon.^2)*(1/(2*K)).*z./mu);
 Psi(rho,phi,z) = exp(1i*u*phig).*exp(-1i*K*z).*(exp(-(q^2)*(rhog.^2)./mu)./mu).*sum(An.*aux1);
 
 Ex(rho,phi,z) = X*Psi;
+
 
 end
